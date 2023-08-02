5d8d5111d78ce7c0cec208dd6319a4f9e37b10aa
Purvi-Bhawsar <141271320+Purvi-Bhawsar@users.noreply.github.com>
2023-08-03 00:47:32 +0530

    Update nodeServer.js

--- nodeServer.js
+++ nodeServer.js
@@ -1,11 +1,10 @@
 const http = require('http');
-const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(100, { 'Content-Type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello, World!');
 });
 
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
